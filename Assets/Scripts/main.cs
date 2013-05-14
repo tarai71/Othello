06c3203210df6d2d7420c009d3b5f179b6f0b9d8
tarai71 <tarai@mail.707.to>
2013-05-14 15:53:37 +0900

    ・"EndGame"コマンド修正

--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -199,6 +199,7 @@ public class main : MonoBehaviour {
 		if(compMenu.getLockType() == menu.LOCK_TYPE.LOCK)
 		{
 			compConnect.Send("{\"type\":\"endgame\"}");
+			compConnect.Send("{\"type\":\"endgame\", \"myid\":\"" + compMenu.GetMyID().ToString() + "\", \"id\":\"" + compMenu.GetYourID().ToString() + "\"}");
 		}
 		compMenu.enabled = true;
 		Application.LoadLevel("Empty");
